b9639a23fbedbc6e816623df367fe49a9a724005
Amelia Hill <49414147+amehi0index@users.noreply.github.com>
2022-03-11 18:46:16 -0800

    Speed up time for menu drawer transition in mobile view

--- js/navbar.js
+++ js/navbar.js
@@ -39,7 +39,7 @@ function animateLinks (navbarLinks) {
             link.style.animation =''
         }
         else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7 + 1}s`;
         }
     })
 }
